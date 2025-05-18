1d7f977ef6864c4b7da53995a4a6931f6ced9341
damiwx3 <pressmmer@gmail.com>
2025-05-18 16:30:29 -0700

    Update bill_payment.model.js

--- models/bill_payment.model.js
+++ models/bill_payment.model.js
@@ -2,10 +2,15 @@ module.exports = (sequelize, DataTypes) => {
   const BillPayment = sequelize.define("BillPayment", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: { type: DataTypes.INTEGER, allowNull: false },
-    serviceType: { type: DataTypes.STRING, allowNull: false }, // e.g. airtime, electricity
+    serviceType: { type: DataTypes.STRING, allowNull: false },
     amount: { type: DataTypes.DECIMAL(12, 2), allowNull: false },
     reference: { type: DataTypes.STRING, unique: true },
     status: { type: DataTypes.STRING, defaultValue: 'pending' }
   });
+
+  BillPayment.associate = (models) => {
+    BillPayment.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+
   return BillPayment;
 };
