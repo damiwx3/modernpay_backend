cba503725f9843a72379b28b2d5ad25b41b9ecda
damiwx3 <pressmmer@gmail.com>
2025-05-18 06:49:03 -0700

    Update otp_code.model.js

--- models/otp_code.model.js
+++ models/otp_code.model.js
@@ -3,9 +3,12 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: { type: DataTypes.INTEGER, allowNull: false },
     code: { type: DataTypes.STRING, allowNull: false },
-    type: { type: DataTypes.STRING, allowNull: false }, // e.g., "login", "transaction"
+    type: { type: DataTypes.STRING, allowNull: false }, // e.g., "login", "transaction", "password_reset"
     expiresAt: { type: DataTypes.DATE, allowNull: false },
-    used: { type: DataTypes.BOOLEAN, defaultValue: false }
+    used: { type: DataTypes.BOOLEAN, defaultValue: false },
+    attempts: { type: DataTypes.INTEGER, defaultValue: 0 },              // NEW
+    blockedUntil: { type: DataTypes.DATE, allowNull: true },             // NEW
   });
+
   return OTPCode;
 };
