963855930571a7b8c27a0add57eccaa8d141a909
damiwx3 <pressmmer@gmail.com>
2025-05-20 11:23:31 -0700

    Update kyc_document.model.js

--- models/kyc_document.model.js
+++ models/kyc_document.model.js
@@ -1,35 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  const KYCDocument = sequelize.define("KYCDocument", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+  const KYCDocument = sequelize.define('KYCDocument', {
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    documentType: {
-      type: DataTypes.STRING,
       allowNull: false,
     },
     documentUrl: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    documentType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     status: {
-      type: DataTypes.ENUM('pending', 'approved', 'rejected'),
+      type: DataTypes.STRING,
       defaultValue: 'pending',
     },
     rejectionReason: {
       type: DataTypes.STRING,
-      allowNull: true,
     },
     submittedAt: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
     },
   });
+
+  KYCDocument.associate = function(models) {
+    KYCDocument.belongsTo(models.User, { foreignKey: 'userId' });
+  };
 
   return KYCDocument;
 };
