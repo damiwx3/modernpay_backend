4060f40417caf01ffa5637f65ccf6b15488302df
damiwx3 <pressmmer@gmail.com>
2025-05-20 20:29:08 -0700

    Update ticket.model.js

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -6,7 +6,12 @@ module.exports = (sequelize, DataTypes) => {
     message: { type: DataTypes.TEXT, allowNull: false },
     status: { type: DataTypes.STRING, defaultValue: 'open' },
     priority: { type: DataTypes.STRING, defaultValue: 'normal' },
-    adminResponse: { type: DataTypes.TEXT, allowNull: true }, 
+    adminResponse: { type: DataTypes.TEXT, allowNull: true },
   });
+
+  Ticket.associate = function(models) {
+    Ticket.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+
   return Ticket;
 };
