e4e09473263c7d48afdc7a59024576111b791df0
damiwx3 <pressmmer@gmail.com>
2025-05-20 19:07:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const maintenanceCheck = require('./middleware/maintenance.middleware');
 const checkMaintenance = require('./middleware/checkMaintenance.middleware'); // <-- Add this line
 const maintenance = require('./middleware/maintenance.middleware');
 const bankRoutes = require('./routes/bank.routes');
+const kycRoutes = require('./routes/kyc.routes');
+const userRoutes = require('./routes/user.routes');
+const savingsRoutes = require('./routes/savings.routes');
 
+app.use('/api/savings', savingsRoutes);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(maintenanceCheck);
 app.use(maintenance);
 app.use(checkMaintenance); // <-- Add this line before loading routes
+app.use('/api/users', userRoutes);
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -38,6 +43,7 @@ app.use('/api/wallets', require('./routes/wallet.routes')); // <-- Added line
 app.use('/api/transactions', require('./routes/transaction.routes'));
 app.use('/api/bills', require('./routes/bill.routes'));
 app.use('/api/bank', bankRoutes);
+app.use('/api/kyc', kycRoutes);
 
 // Catch 404
 app.use((req, res) => res.status(404).json({ error: 'Route not found' }));
