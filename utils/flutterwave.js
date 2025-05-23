4cac393ca6483c55299a23930063e68cc88adddf
damiwx3 <pressmmer@gmail.com>
2025-05-23 13:52:50 -0700

    Update flutterwave.js

--- utils/flutterwave.js
+++ utils/flutterwave.js
@@ -3,19 +3,23 @@ const axios = require('axios');
 const FLW_SECRET_KEY = process.env.FLUTTERWAVE_SECRET_KEY;
 
 exports.initiatePayment = async ({ amount, email, tx_ref, currency = 'NGN' }) => {
-  const response = await axios.post(
-    'https://api.flutterwave.com/v3/payments',
-    {
-      tx_ref,
-      amount,
-      currency,
-      redirect_url: 'https://modernpay-backend.onrender.com/payment-callback',
-      customer: { email },
-      customizations: { title: 'Wallet Funding' }
-    },
-    {
-      headers: { Authorization: `Bearer ${FLW_SECRET_KEY}` }
-    }
-  );
-  return response.data;
+  try {
+    const response = await axios.post(
+      'https://api.flutterwave.com/v3/payments',
+      {
+        tx_ref,
+        amount,
+        currency,
+        redirect_url: 'https://modernpay-backend.onrender.com/payment-callback',
+        customer: { email },
+        customizations: { title: 'Wallet Funding' }
+      },
+      {
+        headers: { Authorization: `Bearer ${FLW_SECRET_KEY}` }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || error.message);
+  }
 };
