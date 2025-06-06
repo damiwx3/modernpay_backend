c25fc7bc6ff7b3546873d3ad2428297f45eb246e
damiwx3 <pressmmer@gmail.com>
2025-06-05 21:10:48 -0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin');
 
 const androidApp = admin.initializeApp({
-  credential: admin.credential.cert(require('./config/firebase-android.json')),
+  credential: admin.credential.cert(require('./firebase-android.json')),
 }, 'android');
 
 const iosApp = admin.initializeApp({
-  credential: admin.credential.cert(require('./config/firebase-ios.json')),
+  credential: admin.credential.cert(require('./firebase-ios.json')),
 }, 'ios');
 
 module.exports = {
