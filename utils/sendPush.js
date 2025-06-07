f1b6e3e9b9d08bdade21992431aad85b599f5e5d
damiwx3 <pressmmer@gmail.com>
2025-06-07 16:55:11 -0700

    Update sendPush.js

--- utils/sendPush.js
+++ utils/sendPush.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 // Initialize Firebase Admin only once in your app!
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ANDROID_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
