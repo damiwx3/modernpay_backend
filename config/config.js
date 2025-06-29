732152966291837ea9bb2024dfe148b1c816b144
Damiwx3 <pressmmer@gmail.com>
2025-06-29 03:31:08 -0700

    fix config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,14 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
     username: process.env.DB_USER,
@@ -14,6 +21,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: 5432,
     dialectOptions: {
       ssl: {
         require: true,
