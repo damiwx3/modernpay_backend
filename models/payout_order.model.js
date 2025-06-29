1541275830130312cb41a06b96bff07591f9ea8d
Damiwx3 <pressmmer@gmail.com>
2025-06-29 03:15:30 -0700

    models/payout_order.model.js

--- models/payout_order.model.js
+++ models/payout_order.model.js
@@ -1,16 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
   const PayoutOrder = sequelize.define('PayoutOrder', {
-    cycleId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    amount: DataTypes.FLOAT,
-    paidAt: DataTypes.DATE,
-    order: DataTypes.INTEGER,
-    status: DataTypes.STRING
-  });
+  cycleId: DataTypes.INTEGER,
+  userId: DataTypes.INTEGER,
+  groupId: { // ✅ Add this field
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  amount: DataTypes.FLOAT,
+  paidAt: DataTypes.DATE,
+  order: DataTypes.INTEGER,
+  status: DataTypes.STRING
+});
 
   PayoutOrder.associate = function(models) {
     PayoutOrder.belongsTo(models.ContributionCycle, { foreignKey: 'cycleId' });
     PayoutOrder.belongsTo(models.User, { foreignKey: 'userId' });
+    PayoutOrder.belongsTo(models.ContributionGroup, { foreignKey: 'groupId' });
   };
 
   return PayoutOrder;
